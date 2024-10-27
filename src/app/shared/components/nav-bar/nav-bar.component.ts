931e3a113c4d57083d091f617029517002c0d77e
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2024-10-27 12:33:34 +0100

    updated navbar

--- src/app/shared/components/nav-bar/nav-bar.component.ts
+++ src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrl: './nav-bar.component.scss',
+  styleUrls: ['./nav-bar.component.scss'],
+  animations: [
+    trigger('slideDown', [
+      state(
+        'open',
+        style({
+          height: '100vh',
+          opacity: 1,
+        })
+      ),
+      state(
+        'closed',
+        style({
+          height: '0px',
+          opacity: 0,
+        })
+      ),
+      transition('closed => open', [animate('300ms ease-in')]),
+      transition('open => closed', [animate('300ms ease-out')]),
+    ]),
+  ],
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
