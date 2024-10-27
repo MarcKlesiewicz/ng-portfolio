b1d7b82d42e122538bae820ea26b67a8927197cf
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2024-10-27 16:21:10 +0100

    setting up inital project filter idea

--- src/app/work/projects.module.ts
+++ src/app/work/projects.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectPageComponent } from './project-page/project-page.component';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
+import { ProjectFilterComponent } from './components/project-filter/project-filter.component';
 
 @NgModule({
-  declarations: [ProjectPageComponent, ProjectCardComponent],
+  declarations: [ProjectPageComponent, ProjectCardComponent, ProjectFilterComponent],
   imports: [CommonModule, ProjectsRoutingModule],
 })
 export class ProjectsModule {}
