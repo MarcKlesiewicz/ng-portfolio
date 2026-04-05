3bfd01048b3f6c92598e4a30a0944768985fda97
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2026-04-05 16:35:10 +0200

    bump to angular 21

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { NgModule, provideZoneChangeDetection } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, isDevMode } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { HttpClient, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { ServiceWorkerModule } from '@angular/service-worker';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { IMAGE_CONFIG } from '@angular/common';
 import { environment } from '@env/environment';
-import { HomeModule } from './home/home.module';
 import { AboutModule } from './about/about.module';
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 import { ProjectsModule } from './work/projects.module';
-import { IMAGE_CONFIG } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,6 +29,7 @@ import { IMAGE_CONFIG } from '@angular/common';
     AppRoutingModule,
   ],
   providers: [
+    provideZoneChangeDetection(),
     provideHttpClient(withInterceptorsFromDi()),
     { provide: IMAGE_CONFIG, useValue: { disableImageSizeWarning: true, disableImageLazyLoadWarning: true } },
   ],
